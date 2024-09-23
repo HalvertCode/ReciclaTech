6e9fa98ecfa0f49698a4a2994413755eb1232819
ElvisCA01 <mark.nicols@hotmail.com>
2024-09-23 15:16:25 -0500

    Bootstrap, Routes, Proyectos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {ProyectosComponent} from "./proyectos/proyectos.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'proyectos', component: ProyectosComponent },
+
+];
