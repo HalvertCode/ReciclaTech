4348d294d89689ef72971f640bd13ddcd166052a
ElvisCA01 <mark.nicols@hotmail.com>
2024-09-23 16:04:12 -0500

    Noticias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import {ProyectosComponent} from "./proyectos/proyectos.component";
+import {NoticiasComponent} from "./noticias/noticias.component";
 
 export const routes: Routes = [
   { path: 'proyectos', component: ProyectosComponent },
+
+  {path: 'noticias', component: NoticiasComponent}
 
 ];
