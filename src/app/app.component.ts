5219e333ad135b72c6be1b1917b3ae2f00124a0a
HalvertCode <hguerrerop1@upao.edu.pe>
2024-09-25 16:04:02 -0500

    inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  standalone: true,
+  imports: [RouterOutlet,FooterComponent,HeaderComponent],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
@@ -14,6 +19,7 @@ export class AppComponent {
     items.forEach(item => {
       item.addEventListener('click', () => {
         const parent = item.parentElement;
+        // @ts-ignore
         parent.classList.toggle('active');
       });
     });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import {ProyectosComponent} from "./proyectos/proyectos.component";
 import {NoticiasComponent} from "./noticias/noticias.component";
 import {EstadisticasComponent} from "./estadisticas/estadisticas.component";
 import {UneteComponent} from "./unete/unete.component";
+import {InicioComponent} from "./inicio/inicio.component";
 
 export const routes: Routes = [
   { path: 'proyectos', component: ProyectosComponent },
   {path: 'estadisticas', component:EstadisticasComponent},
   {path: 'noticias', component: NoticiasComponent},
   {path: 'unete', component: UneteComponent},
+  {path: '', component: InicioComponent},
 
 ];
