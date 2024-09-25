1f04d8f460f7c8d5419775e6ad8279256b82b85b
HalvertCode <hguerrerop1@upao.edu.pe>
2024-09-25 15:30:55 -0500

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "./layout/header/header.component";
-import {FooterComponent} from "./layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reciclatech-app';
+  title = 'Reciclatech';
+
+  ngOnInit() {
+    const items = document.querySelectorAll('.accordion-item h2');
+    
+    items.forEach(item => {
+      item.addEventListener('click', () => {
+        const parent = item.parentElement;
+        parent.classList.toggle('active');
+      });
+    });
+  }
 }
