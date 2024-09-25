2a8dbb0c597e310965e919b19f96934ea92213b1
ElvisCA01 <mark.nicols@hotmail.com>
2024-09-25 14:43:55 -0500

    Responsive Header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {ProyectosComponent} from "./proyectos/proyectos.component";
 import {NoticiasComponent} from "./noticias/noticias.component";
 import {EstadisticasComponent} from "./estadisticas/estadisticas.component";
+import {UneteComponent} from "./unete/unete.component";
 
 export const routes: Routes = [
   { path: 'proyectos', component: ProyectosComponent },
   {path: 'estadisticas', component:EstadisticasComponent},
-  {path: 'noticias', component: NoticiasComponent}
+  {path: 'noticias', component: NoticiasComponent},
+  {path: 'unete', component: UneteComponent},
 
 ];
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {NgbDropdown, NgbDropdownMenu, NgbDropdownToggle} from "@ng-bootstrap/ng-bootstrap";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgbDropdown,
+    NgbDropdownMenu,
+    NgbDropdownToggle,
+    NgClass
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isMenuCollapsed: boolean = true;
 
+  toggleMenu() {
+    this.isMenuCollapsed = !this.isMenuCollapsed;
+  }
 }
